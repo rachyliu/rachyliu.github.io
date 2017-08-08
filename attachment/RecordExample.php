747e676eb35b19986a7aa33a30231c07f111c7d8
Rachel <rachy0321@hotmail.com>
2017-08-08 18:55:35 -0400

    test

--- attachment/RecordExample.php
+++ attachment/RecordExample.php
@@ -1,19 +1,13 @@
-<?php 
-
-/* $postdata = file_get_contents("php://input");
-$request = json_decode($postdata);
-
-$name = $request->name;
-
-echo $name; */
-
-$file="VisitsRecord.json"; 
-
-//$json = json_decode(file_get_contents($file),TRUE); 
-
-//$json['records'][] = array('name'=>'Superman', 'pid'=>4);
-
-//file_put_contents( 'VisitsRecord.json', json_encode( $json ) );
-echo $file;
-
-?>
+{
+  "records": [
+  {
+  "name": "Rachel Liu",
+  "type": "Self",
+  "date": "1502232250578"
+  },
+  {
+  "name": "Ana Trujillo Emparedados y helados",
+  "type": "México D.F.",
+  "date": "1502232250578"
+  }
+}
