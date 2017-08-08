6263aaba94400871e07b4ae6ae37203f4359ed3a
Rachel <rachy0321@hotmail.com>
2017-08-08 18:53:52 -0400

    test

---
+++ attachment/RecordExample.php
@@ -0,0 +1,19 @@
+<?php 
+
+/* $postdata = file_get_contents("php://input");
+$request = json_decode($postdata);
+
+$name = $request->name;
+
+echo $name; */
+
+$file="VisitsRecord.json"; 
+
+//$json = json_decode(file_get_contents($file),TRUE); 
+
+//$json['records'][] = array('name'=>'Superman', 'pid'=>4);
+
+//file_put_contents( 'VisitsRecord.json', json_encode( $json ) );
+echo $file;
+
+?>
